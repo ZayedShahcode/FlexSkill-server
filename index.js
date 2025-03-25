18872574dba0f02d43de96a4644d7147836d9454
ZayedShahcode <reachzayed@proton.me>
2025-03-25 19:58:14 +0530

    cors

--- index.js
+++ index.js
@@ -15,10 +15,20 @@ const app = express();
 
 // Enable CORS for all origins
 app.use(cors({
-    origin: true, // Allow all origins
+    origin: ['https://flexskill.onrender.com', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: '*' // Allow all headers
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Accept',
+        'Origin',
+        'Access-Control-Allow-Headers',
+        'Access-Control-Request-Method',
+        'Access-Control-Request-Headers'
+    ],
+    exposedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json())
